c2f3cdfc01cca329f0bfd6cadaef86b408bc21d9
Khasan Kamalov <info.kamalov@gmail.com>
2023-06-07 14:03:34 +0500

    Added swagger api doc

--- api/v1/proposal/views.py
+++ api/v1/proposal/views.py
@@ -1,5 +1,6 @@
 from rest_framework.views import APIView
 from rest_framework.response import Response
+from drf_yasg.utils import swagger_auto_schema
 from .serializers import ProposalSerializer
 from api.v1.utilis.custom_responses import (
     success_response,
@@ -9,10 +10,10 @@ from api.v1.utilis.custom_responses import (
 
 class ProposalApi(APIView):
 
+    @swagger_auto_schema(request_body=ProposalSerializer)
     def post(self, request, *args, **kwargs):
         serializer = ProposalSerializer(data=request.data)
         if serializer.is_valid():
             serializer.save()
             return Response(success_response())
         return Response(serializer_error_response(serializer.errors))
-
