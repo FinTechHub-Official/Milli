771d83cb7a7ed68618ca26bb8fc92623bfb3afc1
Khasan <info.kamalov@gmail.com>
2023-06-21 10:02:29 +0000

    ..

--- config/settings.py
+++ config/settings.py
@@ -87,7 +87,7 @@ WSGI_APPLICATION = 'config.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
-if not DEBUG:
+if DEBUG:
     DATABASES = {
         'default': {
             'ENGINE': 'django.db.backends.sqlite3',
